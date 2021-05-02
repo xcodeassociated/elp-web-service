e73284842d84208499e97b9e3cc8eeb00564aad1
xcodeassociated <janusz.majchrzak@protonmail.com>
2021-05-02 17:40:12 +0200

    Change app config paths

--- src/app/config/AppConfig.ts
+++ src/app/config/AppConfig.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
   // https://hamashy.asuscomm.com
-  event_service_url: 'http://localhost:8083',
-  user_service_url: 'http://localhost:8082'
+  event_service_url: 'https://api.cnf.sh', //'http://localhost:8083',
+  user_service_url: 'https://api.cnf.sh' //'http://localhost:8082'
 };
